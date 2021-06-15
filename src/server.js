2994e64877bbd43d22f9dc3e779b26dd88de93d3
josé jonathan <josejonathan689@gmail.com>
2021-06-15 00:04:49 -0300

    fim do 2º dia de aula da maratona com o Maik

--- src/server.js
+++ src/server.js
@@ -2,11 +2,14 @@ const express = require('express')
 const server = express()
 const routes = require('./routes')
 
-//usando template engine 
+//usando template engine , estamos dizendo para o motor de visualização que estamos trabalhando com ejs
 server.set('view engine', 'ejs')
 
 //habilitar arquivos estaticos, que não são alterados com frequencia
 server.use(express.static("public"))
+
+//usar o req.body
+server.use(express.urlencoded({extended: true}))
 
 //rotas
 server.use(routes)
